2f736aff7bc1cb3b517041ed6ab32593352ba4f8
나평근[플랫폼개발2팀] <itbuddy@pineone.com>
2023-10-08 19:28:18 +0900

    CoreResponse 생성자 추가

--- src/main/kotlin/com/hanghea99/commerce/api/common/domain/core/CoreResponse.kt
+++ src/main/kotlin/com/hanghea99/commerce/api/common/domain/core/CoreResponse.kt
@@ -1,7 +1,15 @@
 package com.hanghea99.commerce.api.common.domain.core
 
+import com.hanghea99.commerce.api.common.ResultCodeMsg
+
 abstract class CoreResponse<T> (
     var code: String,
     var msg: String,
     open var result: T
-)
+){
+    constructor(resultCodeMsg: ResultCodeMsg, result: T):this(
+        code = resultCodeMsg.code,
+        msg = resultCodeMsg.msg,
+        result = result
+    )
+}

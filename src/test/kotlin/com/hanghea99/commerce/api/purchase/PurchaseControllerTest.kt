41ccae6bd6cf6bea257cb9426da9e811deb942d7
HKMC\H2211843 <9417143@ict-companion.com>
2023-10-10 17:45:46 +0900

    테스트 코드 작성 중

---
+++ src/test/kotlin/com/hanghea99/commerce/api/purchase/PurchaseControllerTest.kt
@@ -0,0 +1,18 @@
+package com.hanghea99.commerce.api.purchase
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+
+@WebMvcTest(PurchaseController::class)
+class PurchaseControllerTest {
+
+    @Test
+    fun getPurchase() {
+    }
+
+    @Test
+    fun getPurchaseService() {
+    }
+}

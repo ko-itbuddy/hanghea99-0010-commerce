cc09e11f83fcc41348d1a8fd9fb0b410f6f85a75
HKMC\H2211843 <9417143@ict-companion.com>
2023-10-20 17:34:02 +0900

    공통 전역 Exception 작업

--- src/main/kotlin/com/hanghea99/commerce/exception/EntityNotFoundException.kt
+++
@@ -1,3 +0,0 @@
-package com.hanghea99.commerce.exception
-
-class EntityNotFoundException(message: String?) : BusinessException(message, ErrorCode.ENTITY_NOT_FOUND)
--- src/main/kotlin/com/hanghea99/commerce/exception/InvalidValueException.kt
+++
@@ -1,6 +0,0 @@
-package com.hanghea99.commerce.exception
-
-class InvalidValueException : BusinessException {
-    constructor(value: String?) : super(value, ErrorCode.INVALID_INPUT_VALUE)
-    constructor(value: String?, errorCode: ErrorCode?) : super(value, errorCode!!)
-}

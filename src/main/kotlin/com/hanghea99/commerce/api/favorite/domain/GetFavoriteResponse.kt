617ddb13b411d8b01aa8c395388af916441fda74
HKMC\H2211843 <9417143@ict-companion.com>
2023-10-24 17:47:04 +0900

    ㅈㅈㅈㅈㅈㅈㅈㅈ????ㅎㅎㅎ

--- src/main/kotlin/com/hanghea99/commerce/api/favorite/domain/GetFavoriteResponse.kt
+++ src/main/kotlin/com/hanghea99/commerce/api/favorite/domain/GetFavoriteResponse.kt
@@ -1,11 +1,16 @@
 package com.hanghea99.commerce.api.favorite.domain
 
 import com.hanghea99.commerce.api.common.ResultCodeMsg
+import com.hanghea99.commerce.api.favorite.domain.result.GetFavoriteResult
 
 data class GetFavoriteResponse (
     var code: String,
     var msg: String,
-    val result: GetItemResult,
+    val result: GetFavoriteResult,
 ){
-    constructor(resultCodeMsg: ResultCodeMsg,result:)
+    constructor(resultCodeMsg: ResultCodeMsg,result: GetFavoriteResult):this(
+        code = resultCodeMsg.code,
+        msg = resultCodeMsg.msg,
+        result = result,
+    )
 }
